b7a27a055a4e91f033558a8f56c7674df1dce888
danielek1337 <daniel.urban234@vp.pl>
2024-12-11 18:18:11 +0100

    add additional effect for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,12 @@ import { motion, useScroll, useTransform } from "framer-motion";
 export default function Header() {
   const { scrollY } = useScroll();
   const yPosition = useTransform(scrollY, [0, 300], [0, -150]);
-  const arrowHeight = useTransform(scrollY, [0, 500], ["10px", "400px"]);
+  const arrowHeight = useTransform(scrollY, [0, 500], ["20px", "400px"]);
+  const arrowOpacity = useTransform(scrollY, [400, 500], [1, 0]);
+  const lineHeight = useTransform(scrollY, [500, 600], ["0px", "400px"]);
 
   return (
-    <header className="lg:bg-hero bg-heroMobile h-screen bg-cover bg-center bg-no-repeat shadow-heroBoxShadow text-center relative">
+    <header className="lg:bg-hero bg-heroMobile h-screen bg-cover bg-center bg-no-repeat shadow-heroBoxShadow text-center relative overflow-hidden">
       <motion.div
         className="absolute m-auto top-1/3 left-0 right-0 w-fit backdrop-blur-sm p-4 rounded-3xl shadow-projectCardShadow"
         style={{ y: yPosition }}
@@ -39,8 +41,20 @@ export default function Header() {
             height: "100%",
           }}
         />
-        <motion.div className="arrow-head border-t-4 border-r-4 border-white  w-4 h-4 transform rotate-[135deg]" />
+        <motion.div
+          className=" border-t-4 border-r-4 border-white w-4 h-4 transform rotate-[135deg]"
+          style={{
+            opacity: arrowOpacity,
+          }}
+        />
       </motion.div>
+      <motion.div
+        className="bg-white absolute bottom-0 left-1/2 transform -translate-x-1/2 "
+        style={{
+          height: "2px",
+          width: lineHeight,
+        }}
+      />
     </header>
   );
 }
